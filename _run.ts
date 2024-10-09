159da32f58e726ec89886c2adaa3d2691c713de0
Stefano Pigozzi <me@steffo.eu>
2024-10-10 01:52:01 +0200

    Fully remove cms

--- _run.ts
+++ _run.ts
@@ -1,10 +1,6 @@
 import {default as site} from "./_config.ts"
-import {default as cms} from "./_cms.ts"
-import {default as adapter} from "lume/cms/adapters/lume.ts"
-
-const app = await adapter({site, cms})
 
 Deno.serve({
     port: 8080,
-    handler: app.fetch
+    handler: site.fetch
 })
