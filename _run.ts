df9d2d4262217167fd4b452aba553245c2c0f074
Stefano Pigozzi <me@steffo.eu>
2024-10-10 02:20:31 +0200

    Revert "Fully remove cms"
    This reverts commit 159da32f58e726ec89886c2adaa3d2691c713de0.

--- _run.ts
+++ _run.ts
@@ -1,6 +1,10 @@
 import {default as site} from "./_config.ts"
+import {default as cms} from "./_cms.ts"
+import {default as adapter} from "lume/cms/adapters/lume.ts"
+
+const app = await adapter({site, cms})
 
 Deno.serve({
     port: 8080,
-    handler: site.fetch
+    handler: app.fetch
 })
