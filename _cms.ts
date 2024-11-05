9912024f5106dc1fff77558bd5d826333c6ced66
Stefano Pigozzi <me@steffo.eu>
2024-11-05 13:02:24 +0100

    Add anime document class

--- _cms.ts
+++ _cms.ts
@@ -210,5 +210,117 @@ cms.collection(
 	]
 )
 
+cms.collection(
+	"anime",
+	"src:anime/*.md",
+	[
+		{
+			name: "name",
+			type: "text",
+			label: "Name",
+			description: "The full name of the anime.",
+			attributes: {
+				required: true,
+				placeholder: "Argon Revelations Catholicism"
+			}
+		},
+		{
+			name: "name_original",
+			type: "text",
+			label: "Original title",
+			description: "The full name of the anime in the native language.",
+			attributes: {
+				placeholder: "Shinseiki Catholicism"
+			}
+		},
+		{
+			name: "name_sort",
+			type: "text",
+			label: "Sort as",
+			description: "If specified, when sorting games alphabetically, the game will be sorted as if this was its actual name.",
+			attributes: {
+				placeholder: "Argon Revelations Catholicism 6.6666"
+			}
+		},
+		{
+			name: "active",
+			type: "checkbox",
+			label: "Now playing",
+			description: "Whether you're currently watching this anime or not. If checked, prominently displays the anime on the home page."
+		},
+		{
+			name: "rating",
+			type: "number",
+			label: "Rating",
+			description: "The rating from 1 to 100 you want to give to the anime. A rating from 1 to 35 is considered negative, from 36 to 65 mixed, from 66 to 95 positive, and from 96 to 100 perfect. A rating of 0 means unscored.",
+			attributes: {
+				placeholder: "Unscored"
+			}
+		},
+		{
+			name: "progress",
+			type: "select",
+			label: "Progress",
+			description: "How much progress you've made in the anime.",
+			options: [
+				{
+					value: "",
+					label: "",
+				},
+				{
+					value: "new",
+					label: "New: you haven't started watching the anime yet.",
+				},
+				{
+					value: "started",
+					label: "Started: you have started the anime, but not reached its end.",
+				},
+				{
+					value: "complete",
+					label: "Complete: you have watched the anime to its end.",
+				},
+				{
+					value: "mastered",
+					label: "Mastered: you have watched everything that this anime has to offer, including side content, like a true fan would do!"
+				},
+			]
+		},
+		{
+			name: "started_on",
+			type: "date",
+			label: "Started on",
+			description: "The date on which you've first started watching the anime.",
+		},
+		{
+			name: "completed_on",
+			type: "date",
+			label: "Completed on",
+			description: "The date on which you've first finished watching the anime.",
+		},
+		{
+			name: "mastered_on",
+			type: "date",
+			label: "Mastered on",
+			description: "The date on which you've achieved mastery of the anime.",
+		},
+		{
+			name: "identifiers",
+			type: "choose-list",
+			label: "Identifiers",
+			description: "Details that unequivocabily and globally identify the anime. Shouldn't be edited manually.",
+			fields: []
+		},
+		{
+			name: "content",
+			type: "markdown",
+			label: "Review",
+			description: "The review to display on the game page.",
+			attributes: {
+				placeholder: "Truly, the most *anime* of all times."
+			}
+		},
+	]
+)
+
 
 export default cms;
